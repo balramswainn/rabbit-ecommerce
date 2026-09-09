453da9d11aa072c8a868ec1433c1e2f90021cee8
balramswainn <balramswain789@gmail.com>
2026-09-09 15:30:07 +0530

    upadated , CartContents, navabar, cartDrawer

--- frontend/src/components/Cart/CartContents.jsx
+++ frontend/src/components/Cart/CartContents.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { RiDeleteBin3Line } from "react-icons/ri";
 
 const CartContents = () => {
   const cartProducts = [
@@ -39,10 +40,19 @@ const CartContents = () => {
             <img src={product.image} alt={product.name} className='w-20 h-24 object-cover mr-4 rounded' />
             <div>
               <h3>{product.name}</h3>
-              <div>{product.size}</div>
-              <div>{product.price}</div>
-              
+              <p className='text-sm text-gray-500'> size: {product.size} | color: {product.color}</p>
+              <div className='flex items-center mt-2'>
+                <button className='border rounded px-2 py-1 text-xl font-medium'>-</button>
+                <span className='mx-4'>{product.quantity}</span>
+                <button className='border rounded px-2 py-1 text-xl font-medium'>+</button>
+              </div>              
             </div>
+          </div>
+          <div>
+            <p className='font-medium'>$ {product.price.toLocaleString()}</p>
+            <button>
+              <RiDeleteBin3Line className="w-6 h-6 mt-2 text-red-600"/>
+            </button>
           </div>
           
         </div>
